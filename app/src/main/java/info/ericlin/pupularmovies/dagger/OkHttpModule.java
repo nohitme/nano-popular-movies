c3c18e980d4879d9f3d366a4ccb2ca6ef69f5fe4
Eric Lin <nohitme@gmail.com>
2018-12-27 21:24:26 -0800

    configure a socket factory to get rid of strict mode warnings

--- app/src/main/java/info/ericlin/pupularmovies/dagger/OkHttpModule.java
+++ app/src/main/java/info/ericlin/pupularmovies/dagger/OkHttpModule.java
@@ -1,10 +1,16 @@
 package info.ericlin.pupularmovies.dagger;
 
 import android.content.Context;
+import android.net.TrafficStats;
 
 import com.google.common.collect.Lists;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
 import java.util.List;
+
+import javax.net.SocketFactory;
 
 import dagger.Module;
 import dagger.Provides;
@@ -15,34 +21,73 @@ import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 
 @Module
-public class OkHttpModule {
+class OkHttpModule {
 
-    @Provides
-    OkHttpClient okHttpClient(Context context) {
+  @Provides
+  OkHttpClient okHttpClient(Context context) {
 
-        OkHttpClient.Builder builder = new OkHttpClient.Builder();
-        // cache
-        int cacheSize = 64 * 1024 * 1024; // 64 MiB
-        Cache cache = new Cache(context.getCacheDir(), cacheSize);
-        builder.cache(cache);
+    OkHttpClient.Builder builder = new OkHttpClient.Builder();
+    // cache
+    int cacheSize = 64 * 1024 * 1024; // 64 MiB
+    Cache cache = new Cache(context.getCacheDir(), cacheSize);
+    builder.cache(cache);
 
-        // interceptors
-        for (Interceptor interceptor : getInterceptors()) {
-            builder.addInterceptor(interceptor);
-        }
+    // just to get rid of strict mode warning
+    // reference:
+    builder.socketFactory(new TagSocketFactory());
 
-        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
-        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
-
-        return builder
-                .addNetworkInterceptor(loggingInterceptor)
-                .build();
+    // interceptors
+    for (Interceptor interceptor : getInterceptors()) {
+      builder.addInterceptor(interceptor);
     }
 
-    private List<Interceptor> getInterceptors() {
-        return Lists.newArrayList(
-                new MovieDbApiKeyInterceptor()
-        );
+    HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+    loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
+
+    return builder.addNetworkInterceptor(loggingInterceptor).build();
+  }
+
+  private List<Interceptor> getInterceptors() {
+    return Lists.newArrayList(new MovieDbApiKeyInterceptor());
+  }
+
+  /**
+   * A socket factory delegates actual call to {@link SocketFactory#getDefault()} and sets the
+   * thread stats to get rid of strict mode warnings.
+   */
+  private static class TagSocketFactory extends SocketFactory {
+
+    // by default okhttp only use parameter-less method
+    // see:
+    // https://square.github.io/okhttp/3.x/okhttp/okhttp3/OkHttpClient.Builder.html#socketFactory-javax.net.SocketFactory-
+    static final String DEFAULT_ERROR_MESSAGE = "okhttp should not call this method";
+
+    @Override
+    public Socket createSocket() throws IOException {
+      Socket socket = SocketFactory.getDefault().createSocket();
+      TrafficStats.setThreadStatsTag(TagSocketFactory.class.hashCode());
+      return socket;
     }
 
+    @Override
+    public Socket createSocket(String host, int port) {
+      throw new UnsupportedOperationException(DEFAULT_ERROR_MESSAGE);
+    }
+
+    @Override
+    public Socket createSocket(String host, int port, InetAddress localHost, int localPort) {
+      throw new UnsupportedOperationException(DEFAULT_ERROR_MESSAGE);
+    }
+
+    @Override
+    public Socket createSocket(InetAddress host, int port) {
+      throw new UnsupportedOperationException(DEFAULT_ERROR_MESSAGE);
+    }
+
+    @Override
+    public Socket createSocket(
+        InetAddress address, int port, InetAddress localAddress, int localPort) {
+      throw new UnsupportedOperationException(DEFAULT_ERROR_MESSAGE);
+    }
+  }
 }
